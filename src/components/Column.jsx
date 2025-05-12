1e8fde13b3d114d0f1d4cacdf8a27a5c0f249500
kardi.ib <kardi.ib@gmail.com>
2025-05-12 17:33:01 +0700

    setup zustand store

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,10 +1,26 @@
-import './Column.css';
-import Task from './Task';
+import { useMemo } from "react";
+import { useStore } from "../store";
+import "./Column.css";
+import Task from "./Task";
+// import { shallow } from "zustand/shallow";
+
 export default function Column({ state }) {
-    return (
-        <div className="column">
-            <p>{state}</p>
-            <Task title="Todo" />
-        </div>
-    )
+  // const tasks = useStore(
+  //   (store) => store.tasks.filter((task) => task.state === state),
+  //   shallow
+  // );
+  const tasks = useStore((store) => store.tasks);
+  const filtered = useMemo(
+    () => tasks.filter((task) => task.state === state),
+    [tasks, state]
+  );
+
+  return (
+    <div className="column">
+      <p>{state}</p>
+      {filtered.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+    </div>
+  );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Task.css';
-import classNames from 'classnames';
+import React from "react";
+import { useStore } from "../store";
+import "./Task.css";
+import classNames from "classnames";
 
-const STATUS = "ONGOING"
-export default function Task({title}) {
+// const STATUS = "ONGOING";
+export default function Task({ title }) {
+  const task = useStore((store) => store.tasks.find((task) => task.title === title));
   return (
-    <div className='task'>
-        <div>{title}</div>
-        <div className='bottomwrapper'>
-            <div></div>
-            <div className={classNames('status', STATUS)}>{STATUS}</div>
-        </div>
+    <div className="task">
+      <div>{title}</div>
+      <div className="bottomwrapper">
+        <div></div>
+        <div className={classNames("status", task.state)}>{task.state}</div>
+      </div>
     </div>
-  )
+  );
 }
