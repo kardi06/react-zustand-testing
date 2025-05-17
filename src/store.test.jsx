9cd84a538ba27f694d97a55a19c1b384f6ac5d61
kardi.ib <kardi.ib@gmail.com>
2025-05-17 23:56:57 +0700

    test(zustand): create unit test add items

--- src/store.test.jsx
+++ src/store.test.jsx
@@ -1,20 +1,33 @@
-import { render } from '@testing-library/react';
+import { render } from "@testing-library/react";
 import { useEffect } from "react";
 import { useStore } from "./store";
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi } from "vitest";
 
+function TestComponent({ selector, effect }) {
+  const items = useStore(selector);
 
-function TestComponent({selector, effect}) {
-    const items = useStore(selector);
+  useEffect(() => effect(items), [items]);
 
-    useEffect(()=> effect(items), [items]);
-    
-    return null;
+  return null;
 }
 
-test('should return default value at the start ', () => {
-    const selector = (store) => store.tasks;
-    const effect = vi.fn();
-    render(<TestComponent selector={selector} effect={effect}/>);
-    expect(effect).toHaveBeenCalledWith([]);
+test("should return default value at the start ", () => {
+  const selector = (store) => store.tasks;
+  const effect = vi.fn();
+  render(<TestComponent selector={selector} effect={effect} />);
+  expect(effect).toHaveBeenCalledWith([]);
+});
+
+test("should add an items to the store and rerun the effect", () => {
+  const selector = (store) => ({ tasks: store.tasks, addTask: store.addTask });
+  const effect = vi.fn().mockImplementation((items) => {
+    if (items.tasks.length === 0) {
+      items.addTask("a", "b");
+    }
+  });
+  render(<TestComponent selector={selector} effect={effect} />);
+  expect(effect).toHaveBeenCalledTimes(2);
+  expect(effect).toHaveBeenCalledWith(
+    expect.objectContaining({ tasks: [{ title: "a", state: "b" }] })
+  );
 });
